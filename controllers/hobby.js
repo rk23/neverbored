55053bcd817b91fe8249b1c591c591add7989290
rk23 <reed.kinning@gmail.com>
2015-12-13 21:47:57 -0800

    more styling, stable

--- controllers/hobby.js
+++ controllers/hobby.js
@@ -7,7 +7,8 @@ router.get('/', function(req, res){
 
 router.get('/:hobby', function(req, res){
    var hobby = req.params.hobby;
-    console.log(hobby);
+
+    //TODO:Capitalize hobby first letter
 
     var fullHobby = {
         name: hobby,
@@ -16,11 +17,29 @@ router.get('/:hobby', function(req, res){
         transport: 0,
         solo: true,
         category: 'adrenaline',
-        quotes: 'Quote about the sport',
-        link: 'http://i.telegraph.co.uk/multimedia/archive/01784/off-piste620_1784286a.jpg'
+        quotes: '"Cronut chia street art gastropub, letterpress chambray affogato put a bird on it keytar. Church-key' +
+        ' keytar blog, everyday carry sriracha +1 viral microdosing. Authentic franzen kinfolk tousled wolf paleo."' +
+        ' - Somebody',
+        link: 'https://i.ytimg.com/vi/61aM0DXpKkc/maxresdefault.jpg'
     };
 
-    res.render('hobby/show', {fullHobby: fullHobby});
+    var skis = {
+        name: 'Skis',
+        price: 343
+    };
+
+    var boots = {
+        name: 'boots',
+        price: 34
+    };
+
+    var poles = {
+        name: 'poles',
+        price: 34
+    };
+    var equipment = [skis, boots, poles];
+
+    res.render('hobby/show', {fullHobby: fullHobby, equipment: equipment});
 });
 
 module.exports = router;
