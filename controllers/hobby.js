3a7b946a62a4db6896a2fad39d05853d09fa6b51
rk23 <reed.kinning@gmail.com>
2015-12-13 20:22:30 -0800

    hobby page styled

--- controllers/hobby.js
+++ controllers/hobby.js
@@ -3,6 +3,24 @@ var express = require('express'),
 
 router.get('/', function(req, res){
     res.render('hobby/index');
-})
+});
+
+router.get('/:hobby', function(req, res){
+   var hobby = req.params.hobby;
+    console.log(hobby);
+
+    var fullHobby = {
+        name: hobby,
+        shortDesc:'Short description',
+        time: 0,
+        transport: 0,
+        solo: true,
+        category: 'adrenaline',
+        quotes: 'Quote about the sport',
+        link: 'http://i.telegraph.co.uk/multimedia/archive/01784/off-piste620_1784286a.jpg'
+    };
+
+    res.render('hobby/show', {fullHobby: fullHobby});
+});
 
 module.exports = router;
